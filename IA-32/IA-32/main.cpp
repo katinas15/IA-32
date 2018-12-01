9dcae44ad88b4f7847227087d959b715a269b70e
Laimonas Janutėnas <laimonas.janutenas@gmail.com>
2018-12-01 13:03:51 +0200

    beveik tiktai reikia isvedima sutvarkyti

--- IA-32/IA-32/main.cpp
+++ IA-32/IA-32/main.cpp
@@ -13,10 +13,6 @@ void decToBin(int num) {
 	cout << "c++ = " << a << endl;
 }
 
-char format[] = "%s %s\n";
-char hello[] = "Hello";
-char world[] = "world";
-
 int main(/*int argc, char** argv*/) {
 
 	/*if (argc < 2) {
@@ -33,7 +29,9 @@ int main(/*int argc, char** argv*/) {
 		push ebx
 		push ecx
 		push edx
+		push ax
 		//-------------------------
+		xor ax,ax		//ax = 0, nustatoma string pozicija
 		mov eax, num
 		xor edx, edx     //edx = 0,			kitaip neveikia dalyba
 		cmp eax,0
@@ -41,18 +39,16 @@ int main(/*int argc, char** argv*/) {
 		mov ebx, eax
 
 		start:
-			mov ebx,eax		//ebx = eax,		perkeliama kad po operacijos nedingtu skaicius
-			mov ecx, 2		//ecx = 2,			kad butu galima dalyba is 
-			div ecx			//eax = num / 2,	dalyba is 2
-			mov ecx,eax		//ecx = eax,		gautas dalybos skaicius perkeliamas i ecx
-			imul ecx,2		//ecx *= 2,			po dalybos gautas skaicius padauginamas is 2 
-			sub ebx,ecx		//ebx = ebx - ecx,	gaunama liekana
-			push ax
-			mov ax, eax
-			mov result, ax
-			push result
-			cmp eax, 0		//tikrina ar likes skaicius daugiau uz 0
-				jg start	//jei ne nulis tesiama toliau
+			mov ebx,eax				//ebx = eax,		perkeliama kad po operacijos nedingtu skaicius
+			mov ecx, 2				//ecx = 2,			kad butu galima dalyba is 
+			div ecx					//eax = num / 2,	dalyba is 2
+			mov ecx,eax				//ecx = eax,		gautas dalybos skaicius perkeliamas i ecx
+			imul ecx,2				//ecx *= 2,			po dalybos gautas skaicius padauginamas is 2 
+			sub ebx,ecx				//ebx = ebx - ecx,	gaunama liekana
+			mov result[ax], ebx		//registras perkeliamas i string
+			inc ax					//ax++ keiciama string pozicija
+			cmp eax, 0				//tikrina ar likes skaicius daugiau uz 0
+				jg start			//jei ne nulis tesiama toliau
 			//mov edx,ebx
 			//int 21h
 		end:	
@@ -61,6 +57,7 @@ int main(/*int argc, char** argv*/) {
 		pop ebx
 		pop ecx
 		pop edx
+		pop ax
 	}
 	std::cout << "assembly = "<< result;
 	getchar();
